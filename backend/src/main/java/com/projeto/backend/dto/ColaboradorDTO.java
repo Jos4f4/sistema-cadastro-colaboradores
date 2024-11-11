4ce21dfab22c30f7956fc3ea86522a72a243edfd
Josaf4 <josafa.soares2909@gmail.com>
2024-11-11 00:47:34 -0400

    Implementation Exceptions in backend

--- backend/src/main/java/com/projeto/backend/dto/ColaboradorDTO.java
+++ backend/src/main/java/com/projeto/backend/dto/ColaboradorDTO.java
@@ -36,29 +36,47 @@ public class ColaboradorDTO {
 		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
 	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 	public String getDepartamento() {
 		return departamento;
 	}
 
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
 
 	public String getCargo() {
 		return cargo;
 	}
 
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
 
 	public Double getSalario() {
 		return salario;
 	}
 
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
 
 	public String getDescricaocargo() {
 		return descricaocargo;
 	}
-	
+
+	public void setDescricaocargo(String descricaocargo) {
+		this.descricaocargo = descricaocargo;
+	}
 }
--- backend/src/main/java/com/projeto/backend/entities/ColaboradorEntity.java
+++ backend/src/main/java/com/projeto/backend/entities/ColaboradorEntity.java
@@ -87,5 +87,59 @@ public class ColaboradorEntity {
 	public void setDescricaocargo(String descricaocargo) {
 		this.descricaocargo = descricaocargo;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cargo == null) ? 0 : cargo.hashCode());
+		result = prime * result + ((departamento == null) ? 0 : departamento.hashCode());
+		result = prime * result + ((descricaocargo == null) ? 0 : descricaocargo.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((salario == null) ? 0 : salario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ColaboradorEntity other = (ColaboradorEntity) obj;
+		if (cargo == null) {
+			if (other.cargo != null)
+				return false;
+		} else if (!cargo.equals(other.cargo))
+			return false;
+		if (departamento == null) {
+			if (other.departamento != null)
+				return false;
+		} else if (!departamento.equals(other.departamento))
+			return false;
+		if (descricaocargo == null) {
+			if (other.descricaocargo != null)
+				return false;
+		} else if (!descricaocargo.equals(other.descricaocargo))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (salario == null) {
+			if (other.salario != null)
+				return false;
+		} else if (!salario.equals(other.salario))
+			return false;
+		return true;
+	}
 }
